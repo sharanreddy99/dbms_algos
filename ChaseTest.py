2219c26c51e5392e689bf2a2e695e55f8d3488e5
Sharan Reddy <sharansaikonda99@gmail.com>
2023-11-27 18:16:14 -0500

    Chase Test

--- ChaseTest.py
+++ ChaseTest.py
@@ -4,6 +4,69 @@ from Utils import *
 from itertools import permutations
 from copy import deepcopy
 
+def areMatricesSame(mat1, mat2):
+    for i in range(len(mat1)):
+        for j in range(len(mat1[0])):
+            if mat1[i][j] != mat2[i][j]:
+                return False
+    
+    return True
+
+def iterative(matrix, f):
+    isSecondTimeUnchanged = None
+    idx = 1
+    while True:
+        new_matrix = deepcopy(matrix)
+        isChanged = False
+
+        for X, Y in f:
+            unqTuples = {}
+            for row in new_matrix:
+                left = ''
+                for char in X:
+                    left += row[R.index(char)]
+
+                right = row[R.index(Y)]
+
+                if left in unqTuples:
+                    if (unqTuples[left]) != 1 and len(right) == 1:
+                        unqTuples[left] = right
+                else:
+                    unqTuples[left] = right
+
+            for row in new_matrix:
+                left = ''
+                for char in X:
+                    left += row[R.index(char)]
+
+                right = row[R.index(Y)]
+                if len(row[R.index(right[0].upper())]) != 1:
+                    row[R.index(right[0].upper())] = unqTuples[left]
+            
+            if not (areMatricesSame(matrix, new_matrix)):
+                print('Step {2}: After applying FD {0}->{1}, matrix is: '.format(X, Y, idx))
+                idx += 1
+                printMatrix(new_matrix)
+                matrix = deepcopy(new_matrix)
+                isChanged =True
+                isSecondTimeUnchanged = None
+            else:
+                isChanged = False
+
+        if not isChanged:
+            if isSecondTimeUnchanged is None:
+                print('No changes testing all the FDs once')
+                isSecondTimeUnchanged = True
+            else:
+                return new_matrix
+    
+        
+def printMatrix(matrix):
+    print('\t'.join(R))
+    for row in matrix:
+        print('\t'.join(row))
+    print()
+
 def chaseTest(f, RSet, R, isPrint = True):
     matrix = []
     for i in range(len(RSet)):
@@ -17,39 +80,16 @@ def chaseTest(f, RSet, R, isPrint = True):
             matrix[i][R.index(char)] = char.lower()
 
     if isPrint:
-        print('\t'.join(R))
-        for row in matrix:
-            print('\t'.join(row))
-        print()
+        printMatrix(matrix)
 
-    for newF in permutations(f):
-        new_matrix = deepcopy(matrix)
-        for X, Y in newF:
-            unqTuples = {}
-            for row in new_matrix:
-                left = ''
-                for char in X:
-                    left += row[R.index(char)]
-
-                for char in Y:
-                    right += row[R.index(char)]
-
-                if left in unqTuples:
-                    if not left.isalpha():
-                        old_right = unqTuples[left]
-                        
-
-                else:
-
-                unqTuples[left] = right
-
-
-
-
-    return True 
+    new_matrix = iterative(matrix, f)
+    return True if any([all([len(char) == 1 for char in row]) for row in new_matrix]) else False
 
 if __name__ == "__main__":     
-    f = set([('A', 'B'), ('B', 'C'), ('CD', 'A')])
-    RSet = [['A','D'], ['A','C'], ['B','C','D']]
+    # f = list([('A', 'B'), ('BC', 'D'), ('E', 'C'), ('D', 'A'), ('D', 'E'), ('C', 'F')])
+    # RSet = [['A','C'], ['A','B', 'D'], ['D','E','F'], ['C', 'F']]
+    # R = list('ABCDEF')
+    f = list([('A', 'B'), ('A', 'C'), ('A', 'D'), ('B', 'D')])
+    RSet = [['A','B'], ['B', 'C'],['C','D']]
     R = list('ABCD')
     print(chaseTest(f, RSet, R))
